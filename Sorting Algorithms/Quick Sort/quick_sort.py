b91b97ab6c19d933b134b6770907f0ec28c9e480
anuragkj <f20200128@dubai.bits-pilani.ac.in>
2023-06-19 15:02:39 +0530

    Create quick_sort.py

---
+++ Sorting Algorithms/Quick Sort/quick_sort.py
@@ -0,0 +1,36 @@
+def partition(arr, low, high):
+ 
+    pivot = arr[low]
+    i = low - 1
+    j = high + 1
+ 
+    while (True):
+ 
+        i += 1
+        while (arr[i] < pivot):
+            i += 1
+
+        j -= 1
+        while (arr[j] > pivot):
+            j -= 1
+ 
+        if (i >= j):
+            return j
+ 
+        arr[i], arr[j] = arr[j], arr[i]
+ 
+ 
+
+def quickSort(arr, low, high):
+    if (low < high): 
+        pi = partition(arr, low, high)
+        quickSort(arr, low, pi)
+        quickSort(arr, pi + 1, high)
+ 
+ 
+ 
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quickSort(arr, 0, n - 1)
+print(arr)
+ 
